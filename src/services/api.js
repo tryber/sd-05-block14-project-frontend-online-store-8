ce50a07620bae7ae070271f2cda6bf9a010d40eb
Giuliano Peixoto <giulianopeixoto@gmail.com>
2020-07-24 15:58:29 -0300

    inicial commit / api requirement

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,22 @@
 export async function getCategories() {
   // implement here
+  const url = 'https://api.mercadolibre.com/sites/MLB/categories';
+  return await fetch(url)
+  .then(response => response.json());
 }
-
+  
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // implement here
+  if(categoryId) {
+  return await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`)
+  .then((response) => response.json()); 
+
+  } else if(query) {
+  return await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`)
+  .then((response) => response.json());
+  
+  } else {
+  return await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
+  .then((response) => response.json());
+  }
 }
