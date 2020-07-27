bd2dc73b6444e03fdf7c0295b45f52f015db0de9
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-07-27 17:08:02 -0300

    codeclimate hotfix

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -4,52 +4,69 @@ import * as API from '../services/api';
 class ListItem extends React.Component {
   constructor(props) {
     super(props);
-    this.state ={
+    this.state = {
       search: [],
       value: '',
       categoria: '',
       result: [],
-    }
-    this.handleChange = this.handleChange.bind(this); 
-    this.handleClickCategory = this.handleClickCategory.bind(this); 
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClickCategory = this.handleClickCategory.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
   }
 
+  async searchGetCategories() {
+    this.setState(
+      {
+        search: await API.getCategories(),
+      }
+    );
+  }
   async componentDidMount() {
-    this.setState({
-      search: await API.getCategories()
-    })
+    this.searchGetCategories();
   }
 
   handleChange(e) {
-    this.setState({
-      value: e.target.value
-    })
+    this.setState(
+      {
+        value: e.target.value,
+      }
+    );
   }
 
   handleClickCategory(e) {
     this.setState({
-      categoria: e.target.value
+      categoria: e.target.value,
     })
   }
 
   async handleSearch() {
-    const resultado = await API.getProductsFromCategoryAndQuery(this.state.categoria, this.state.value);
+    const resultado = await API.getProductsFromCategoryAndQuery(
+      this.state.categoria, this.state.value
+    );
     this.setState({
       result: resultado.results,
     });
   }
 
-  render(){
-    const {search, result} = this.state;
+  render() {
+    const { search, result } = this.state;
     return (
       <div>
-        <input type="text" onChange={this.handleChange}/><button type="button" onClick={this.handleSearch}>Buscar</button>
+        <input type="text" onChange={this.handleChange} />
+        <button type="button" onClick={this.handleSearch}>
+          Buscar
+        </button>
         <p data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</p>
         <ul>
           {search.map((category, index) => 
             <li key={ index }>
-              <button type='button' value={category.id} onClick={this.handleClickCategory}>{ category.name }</button>
+              <button 
+                type='button' 
+                value={category.id} 
+                onClick={this.handleClickCategory}>
+                { category.name }
+              </button>
             </li>)
           }
         </ul>
