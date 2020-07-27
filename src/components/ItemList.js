fea07ab528cabb907f059f7e863346358d2e24e2
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-07-27 16:34:19 -0300

    Alterado Componente ItemList

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,19 +1,73 @@
-import React, {useState} from 'react';
-import * as api from '../service/api';
-function ItemList() {
+import React from 'react';
+import * as API from '../services/api';
 
-  const [value, setValue] = useState('');
-  
-  
+class ListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      search: [],
+      value: '',
+      categoria: '',
+      result: [],
+    }
+    this.handleChange = this.handleChange.bind(this); 
+    this.handleClickCategory = this.handleClickCategory.bind(this); 
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  async componentDidMount() {
+    this.setState({
+      search: await API.getCategories()
+    })
+  }
+
+  handleChange(e) {
+    this.setState({
+      value: e.target.value
+    })
+  }
+
+  handleClickCategory(e) {
+    this.setState({
+      categoria: e.target.value
+    })
+  }
+
+  async handleSearch() {
+    const resultado = await API.getProductsFromCategoryAndQuery(this.state.categoria, this.state.value);
+    this.setState({
+      result: resultado.results,
+    });
+  }
+
+  render(){
+    const {search, result} = this.state;
     return (
-      <div>
-        <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="search" />
-        <p className="search-text" data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
-        <span>{value}</span>
-      </div>
-    );
+      <>
+        <input type="text" onChange={this.handleChange}/><button type="button" onClick={this.handleSearch}>Buscar</button>
+        <p data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</p>
+        <ul>
+          {search.map((category, index) => 
+            <li key={ index }>
+              <button type='button' value={category.id} onClick={this.handleClickCategory}>{ category.name }</button>
+            </li>)
+          }
+        </ul>
+        <div>
+          {result.map((category, index) =>
+            <div key={index}>
+            <span >{category.title}</span>
+            <img src= {category.thumbnail} />
+            <span >R$ {category.price}</span>
+            </div>            
+          )}
+        </div>
+      </>
+    )
+  }
+
+  
+
 }
 
-export default ItemList;
+export default ListItem;
