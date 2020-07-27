ee6d8777372958e1d5532866f5167bab1333d9b9
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-07-27 17:25:54 -0300

    codeclimate hotfix

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -15,38 +15,34 @@ class ListItem extends React.Component {
     this.handleSearch = this.handleSearch.bind(this);
   }
 
-  async searchGetCategories() {
-    this.setState(
-      {
-        search: await API.getCategories(),
-      }
-    );
-  }
   async componentDidMount() {
     this.searchGetCategories();
   }
 
+  async searchGetCategories() {
+    this.setState(
+      {search: await API.getCategories(),}
+    );
+  }
+
   handleChange(e) {
     this.setState(
-      {
-        value: e.target.value,
-      }
+      {value: e.target.value,}
     );
   }
 
   handleClickCategory(e) {
-    this.setState({
-      categoria: e.target.value,
-    })
+    this.setState(
+      {categoria: e.target.value,}
+    );
   }
 
   async handleSearch() {
-    const resultado = await API.getProductsFromCategoryAndQuery(
-      this.state.categoria, this.state.value
-    );
-    this.setState({
-      result: resultado.results,
-    });
+    const resultado = await
+      API.getProductsFromCategoryAndQuery(this.state.categoria, this.state.value);
+      this.setState({
+        result: resultado.results,
+      });
   }
 
   render() {
@@ -54,18 +50,18 @@ class ListItem extends React.Component {
     return (
       <div>
         <input type="text" onChange={this.handleChange} />
-        <button type="button" onClick={this.handleSearch}>
-          Buscar
-        </button>
-        <p data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</p>
+        <button type="button" onClick={this.handleSearch}>Buscar</button>
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
         <ul>
-          {search.map((category, index) => 
-            <li key={ index }>
-              <button 
-                type='button' 
-                value={category.id} 
+          {search.map((category, index) =>
+            <li key={index}>
+              <button
+                type="button"
+                value={category.id}
                 onClick={this.handleClickCategory}>
-                { category.name }
+                {category.name}
               </button>
             </li>)
           }
@@ -76,7 +72,7 @@ class ListItem extends React.Component {
             <span >{category.title}</span>
             <img src= {category.thumbnail} />
             <span >R$ {category.price}</span>
-            </div>            
+            </div>     
           )}
         </div>
       </div>
