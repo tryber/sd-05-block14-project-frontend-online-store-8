83b7466fd71c74bb8c1482e27fd6438ee39b62e3
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-07-29 00:27:05 -0300

    codeclimate

--- src/components/FreeShipping.js
+++ src/components/FreeShipping.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class FreeShipping extends React.Component {
   render() {
-    if (this.props.free === true) return <span data-testid="free-shipping">Frete Grátis</span>
+    if (this.props.free === true) return <span data-testid="free-shipping">Frete Grátis</span>;
     return (
       <span />
     );
