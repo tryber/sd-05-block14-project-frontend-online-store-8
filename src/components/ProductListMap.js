372157e4d900e32da57b50877fbba526174268a7
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-07-29 17:31:17 -0300

    codeclimate

--- src/components/ProductListMap.js
+++ src/components/ProductListMap.js
@@ -1,18 +1,18 @@
-import React  from 'react';
+import React from 'react';
 import ProductInfo from './ProductInfo';
 
 class ProductListMap extends React.Component {
 
-render() {
-  const productsList = this.props.productsList;
-  return (
-    <div className="productsList">
-      <ul>
-        {productsList.map((product) => <ProductInfo key={product.id} product={product} />)}
-      </ul>
-    </div>
-  );
-}
+  render() {
+    const productsList = this.props.productsList;
+    return (
+      <div className="productsList">
+        <ul>
+          {productsList.map((product) => <ProductInfo key={product.id} product={product} />)}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default ProductListMap;
